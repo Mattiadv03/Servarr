c2276c03d234edd5eb7f326bde9db6761d90fa5f
Dalla Valle Mattia <85295614+Mattiadv03@users.noreply.github.com>
2024-01-20 16:38:14 +0100

    Added /

--- filesystem.sh
+++ filesystem.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 groupadd -g 1500 media
 
-mkdir data
-mkdir data/torrents data/media data/config
-mkdir data/config/qbittorrent data/config/prowlarr data/config/radarr data/config/sonarr data/config/lidarr data/config/readarr
-mkdir data/torrents/movies data/torrents/tv-series data/torrents/music data/torrents/books
-mkdir data/media/movies data/media/tv-series data/media/music data/media/books
+mkdir /data
+mkdir /data/torrents /data/media /data/config
+mkdir /data/config/qbittorrent /data/config/prowlarr /data/config/radarr /data/config/sonarr /data/config/lidarr /data/config/readarr
+mkdir /data/torrents/movies /data/torrents/tv-series /data/torrents/music /data/torrents/books
+mkdir /data/media/movies /data/media/tv-series /data/media/music /data/media/books
 
 chown :media data/ -R
 chmod 775 data/ -R
