2334477059444e2a3165a6ded7868e1ddcc5fa8b
Dalla Valle Mattia <85295614+Mattiadv03@users.noreply.github.com>
2024-01-21 21:03:53 +0100

    Added chown

--- createUser.sh
+++ createUser.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 useradd -u 1510 -g media qbittorrent
-passwd qbittorrent
+chown qbittorrent /docker/qbittorrent /data/torrents -R
 
 useradd -u 1520 -g media prowlarr
-passwd prowlarr
+chown prowlarr /docker/prowlarr
 
 useradd -u 1530 -g media radarr
-passwd radarr
+chown radarr /docker/radarr /data/media/movies
 
 useradd -u 1540 -g media sonarr
-passwd sonarr
+chown sonarr /docker/sonarr /data/media/tv-series
 
 useradd -u 1550 -g media readarr
-passwd readarr
+chown readarr /docker/readarr /data/media/music
 
 useradd -u 1560 -g media lidarr
-passwd lidarr
+chown lidarr /docker/lidarr /data/media/books
